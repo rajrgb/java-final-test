82538fe560a8ac0d72e9fd0a12bf155fe6d6b6ef
Raj <kumarraj7198@gmail.com>
2022-03-04 17:28:26 +0530

    4/3/22 commit

---
+++ test/src/main/java/com/example/demo/TutorialRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
+    List<Tutorial> findByPublished(boolean published);
+    List<Tutorial> findByTitleContaining(String title);
+}
